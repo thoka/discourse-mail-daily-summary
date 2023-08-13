e72488a41f6b838f762b51b84b05b4f86d08fe4f
Thomas Kalka <thomas.kalka@gmail.com>
2023-08-13 09:51:09 +0200

    add config option to serialized custom fields again
    otherwise the js code to update the model does not get initialized with the current value

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-mail-daily-summary
 # about: Send a daily summary email.
-# version: 0.0
+# version: 0.0.1
 # author: Thomas Kalka thomas.kalka@gmail.com
 # url: https://www.github.com/thoka/discourse-mail-daily-summary
 
@@ -8,12 +8,12 @@
 # https://www.github.com/joebuhlig/discourse-mlm-daily-summary
 
 enabled_site_setting :mlm_daily_summary_enabled
+DiscoursePluginRegistry.serialized_current_user_fields << "user_mlm_daily_summary_enabled"
 
 load File.expand_path('../lib/discourse_mail_daily_summary/engine.rb', __FILE__)
 
 after_initialize do
   # TODO change name? this name is historical
-  register_editable_user_custom_field :user_mlm_daily_summary_enabled
   User.register_custom_field_type('user_mlm_daily_summary_enabled', :boolean) 
-  add_to_serializer(:user, :user_mlm_daily_summary_enabled) { self.custom_fields['user_mlm_daily_summary_enabled'] }
+  register_editable_user_custom_field :user_mlm_daily_summary_enabled
 end
