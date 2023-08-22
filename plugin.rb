7e250fdb582c2646b06f8d218210a276d8a710f0
Thomas Kalka <thomas.kalka@gmail.com>
2023-08-22 07:36:50 +0200

    more renaming

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-mail-daily-summary
 # about: Send a daily summary email.
-# version: 0.0.4
+# version: 0.0.5
 # author: Thomas Kalka thomas.kalka@gmail.com
 # url: https://www.github.com/thoka/discourse-mail-daily-summary
 
