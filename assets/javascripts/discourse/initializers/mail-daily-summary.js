13525f2cb7e7b4e0f264b7b2d630897f26be784c
Richard <richard@communiteq.com>
2026-04-22 16:02:30 +0000

    Set plugin icon

--- assets/javascripts/discourse/initializers/mail-daily-summary.js
+++ assets/javascripts/discourse/initializers/mail-daily-summary.js
@@ -1,9 +1,12 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
+const PLUGIN_ID = "discourse-mail-daily-summary";
+
 export default {
   name: "mail_daily_summary",
   initialize(container) {
     withPluginApi("1.2.0", (api) => {
+      api.setAdminPluginIcon(PLUGIN_ID, "envelope");
       api.addSaveableCustomFields("emails");
     });
   },
